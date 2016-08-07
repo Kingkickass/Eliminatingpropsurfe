d5f14f3a15ae5f57b27e0f24d8d8b74a7bb97cfe
Kingkickass <xaola3609@yahoo.com>
2016-08-07 21:00:57 +0100

    Add files via upload

--- sv_init.lua
+++ sv_init.lua
@@ -7,7 +7,7 @@ PLUGIN_SHARED = PLUGIN
 
 function PLUGIN.Physgunuse(ply,ent)
                        if ent:IsValid() && ent:GetCollisionGroup(COLLISION_GROUP_NONE) then
-                      ent:SetCollisionGroup(COLLISION_GROUP_WEAPON)
+                      ent:SetCollisionGroup(COLLISION_GROUP_WORLD)
                   else 
         return false
      end
@@ -25,5 +25,14 @@ hook.Add( "PhysgunPickup", "Ppickup", PLUGIN.Physgunuse )
 
 hook.Add( "PhysgunDrop", "Pdrop", PLUGIN.Physgunndrop )
 
+cityrp.command.add("propprotection", "a", 2, function(ply , arg)
+local target = ply
+arg[1] = target
+ if arg[2] == "on" then
+ 	target:SetNWBoll("DisableProppush", false)
+ end
+ if arg[2] == "off" then
+ 	target:SetNWBoll("DisableProppush",true)
+ end
 
 cityrp.plugin.register(PLUGIN) 
